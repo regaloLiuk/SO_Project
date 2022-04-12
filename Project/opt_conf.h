68367297f6b9e45608f8158960c388f607fb4a4a
Luca <87311356+regaloLiuk@users.noreply.github.com>
2022-04-12 18:10:49 +0200

    Add files via upload

---
+++ Project/opt_conf.h
@@ -0,0 +1,9 @@
+#define POP_SIZE 1000
+#define sim_time 5
+#define max_reject 3
+#define nof_invites 3
+#define MSG_LEN 10
+#define P2 30	//preferenza grupppi da 2
+#define P3 30	//preferenza grupppi da 3
+#define P4 40	//preferenza grupppi da 4
+
